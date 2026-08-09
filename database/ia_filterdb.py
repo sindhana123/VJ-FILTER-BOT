d29c1b0c5a2106a4df01747de5c7e86f0b230dec
sindhana123 <sindhana9489879284@gmail.com>
2026-08-09 18:47:11 +0530

    Update file saving logic and change credits

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -25,7 +25,7 @@ async def save_file(media):
     
     file_id = unpack_new_file_id(media.file_id)
     file_name = clean_file_name(media.file_name)
-    new_file_name = f"@VJ_Bots {file_name}"
+    new_file_name = f"@Toonworld4all_Tamil {file_name}"
     
     file = {
         'file_id': file_id,
